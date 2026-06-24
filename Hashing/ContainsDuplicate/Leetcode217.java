dc29b65426e900879fddd21379c7c2a2b08814d1
Daksh Dharmani <daksh.v.dharmani@gmail.com>
2026-06-24 19:30:50 +0530

    Add ContainsDuplicate class with user input handling
    Implemented a solution to check for duplicates in an array using HashSet.

--- Hashing/ContainsDuplicate/Leetcode217.java
+++ Hashing/ContainsDuplicate/Leetcode217.java
@@ -1,1 +1,41 @@
+//Optimal Solution 
 
+package HashSet;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner; 
+
+public class ContainsDuplicate {
+    public static void main(String args[]){
+        System.out.println("Enter the array: "); 
+        Scanner sc = new Scanner(System.in); 
+
+        ArrayList<Integer> array = new ArrayList<>(); 
+
+        while (true) {
+            int x = sc.nextInt();
+
+            if (x == -1)
+                break;
+
+            array.add(x); 
+        }
+
+
+        sc.close(); 
+
+        HashSet<Integer> hs = new HashSet<>(); 
+
+        for(int i = 0; i<array.size(); i++){
+
+            if(hs.contains(array.get(i)))
+                System.out.println("The array contains duplicates."); 
+        
+            hs.add(array.get(i)); 
+        }
+    }
+}
+
+//Time complexity = O(n)
+//Space complexity = O(n) <-worst case. 
