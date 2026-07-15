74bd2df5a291e80f49b3922562bcd33da23d6d34
Daksh Dharmani <daksh.v.dharmani@gmail.com>
2026-07-15 19:33:07 +0530

    Create BruteForce.java

---
+++ TwoPtr/VariableSlidingWindow/Leetcode1695/BruteForce.java
@@ -0,0 +1,28 @@
+//Brute Force solution 
+
+class Solution {
+    public int maximumUniqueSubarray(int[] nums) {
+        HashSet<Integer> hs = new HashSet<>(); 
+
+        int left = 0; 
+        int sum = 0, maxSum = 0; 
+
+        for(int right = 0; right<nums.length; right++){
+            while(hs.contains(nums[right])){
+                hs.remove(nums[left]); 
+                sum-= nums[left]; 
+                left++; 
+            }
+
+            hs.add(nums[right]); 
+            sum+=nums[right]; 
+
+            maxSum = Math.max(maxSum, sum); 
+        }
+
+        return maxSum; 
+    }
+}
+
+//Time complexity = O(n) <-traverses through the entire array once. 
+//Space complexity = O(n) <-all the elements are once inserted.  
