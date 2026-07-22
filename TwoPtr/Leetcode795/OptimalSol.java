77ded2c6522c02002a3d14eec4e355585aa18fdc
Daksh Dharmani <daksh.v.dharmani@gmail.com>
2026-07-22 17:52:53 +0530

    Add optimal solution for bounded maximum subarrays
    Implement optimal solution for counting subarrays with bounded maximum.

---
+++ TwoPtr/Leetcode795/OptimalSol.java
@@ -0,0 +1,29 @@
+//Optimal Solution 
+
+class Solution {
+    public int numSubarrayBoundedMax(int[] nums, int left, int right) {
+
+        int lastValid = -1;     // Last index where left <= nums[i] <= right
+        int lastInvalid = -1;   // Last index where nums[i] > right
+
+        int ans = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] > right) {
+                lastInvalid = i;
+            }
+
+            if (nums[i] >= left && nums[i] <= right) {
+                lastValid = i;
+            }
+
+            ans += Math.max(0, lastValid - lastInvalid);
+        }
+
+        return ans;
+    }
+}
+
+//Time complexity = O(n)
+//Space complexity = O(1)
