e9046cbff67b489fe1c2d1b920f5cb09eac91e9b
Daksh Dharmani <daksh.v.dharmani@gmail.com>
2026-08-04 20:26:31 +0530

    Starting recursion with Fibonacci Series

---
+++ DP-Recursion/Fibonacci/Leetcode509/OptimalSol.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public int calc(int n){
+        if(n<=1)
+            return n; 
+
+        return calc(n-1)+calc(n-2); 
+    }
+    public static void main(String args[]) {
+
+        System.out.print("Enter the number: "); 
+
+        Scanner sc = new Scanner(System.in); 
+
+        int n = sc.nextInt();
+
+        int ans = calc(n);
+        
+        System.out.print(ans); 
+         
+    }
+}
