5e2ea992762f3969f6230c65fd1213f5bf792fa8
Daksh Dharmani <daksh.v.dharmani@gmail.com>
2026-08-05 09:29:55 +0530

    Adding solution for longest Fib subarray

---
+++ DP-Recursion/Fibonacci/Leetcode3708/OptimalSol.java
@@ -0,0 +1,38 @@
+package DP-Recursion.Fibonacci.Leetcode3708;
+
+import java.util.Scanner; 
+
+public class OptimalSol {
+    public static void main(String args[]){
+        System.out.println("Enter the array"); 
+
+        Scanner sc = new Scanner(System.in); 
+
+        int x = sc.nextInt(); 
+        int curr = 0; 
+
+        int[] arr = new int[100]; 
+
+        while(x!=-1&&curr<100){
+            arr[curr] = x; 
+            x = sc.nextInt(); 
+            curr++; 
+        }
+
+        sc.close();
+        
+        int count = 2, maxCount = 2; 
+        
+        for(int i = 2; i<curr; i++){
+            if(arr[i]==arr[i-1]+arr[i-2])
+                count++; 
+
+            else   
+                count = 2; 
+
+            maxCount = Math.max(maxCount, count); 
+        }
+
+        System.out.println("The longest fibonacci subarray is of " + maxCount + " size"); 
+    }
+}
