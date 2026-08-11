9dc004bee6acad780354388e6a1c9b64bb55e24f
Daksh Dharmani <daksh.v.dharmani@gmail.com>
2026-08-11 23:04:41 +0530

    Odd even optimal sol in list

---
+++ LinkedList/SlowFastPtr/Leetcode328/OptimalSol.java
@@ -0,0 +1,40 @@
+//Optimal Solution 
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+
+        if(head == null || head.next == null)
+            return head; 
+            
+        ListNode odd = head; 
+        //head preserves the start of odd list 
+        ListNode even = head.next; 
+
+        ListNode evens = even; //start of even list 
+
+        while(even!=null&&even.next!=null&&odd!=null&&odd.next!=null){
+            odd.next = odd.next.next; 
+            odd = odd.next; 
+
+            even.next = even.next.next; 
+            even = even.next; 
+        }
+
+        odd.next = evens; 
+
+        return head; 
+    }
+}
+
+//Time complexity = O(n) <-traverses the list once 
+//Space complexity = O(1)
