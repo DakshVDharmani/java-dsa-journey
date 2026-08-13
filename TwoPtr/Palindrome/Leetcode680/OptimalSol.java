480dc8d233aeadfcd65befd2c7ba4f7f132ba37e
Daksh Dharmani <daksh.v.dharmani@gmail.com>
2026-08-13 11:33:43 +0530

    Valid Palindrome II

---
+++ TwoPtr/Palindrome/Leetcode680/OptimalSol.java
@@ -0,0 +1,35 @@
+//Optimal Solution 
+
+class Solution {
+    public boolean validPalindrome(String s) {
+        int left = 0, right = s.length()-1; 
+
+        while(left<right){
+            if(s.charAt(left)==s.charAt(right)){
+                left++; 
+                right--; 
+            }
+
+            else{
+                return isPalindrome(s, left+1, right)||isPalindrome(s, left, right-1); 
+            }
+        }
+
+        return true; 
+    }
+
+    public boolean isPalindrome(String s, int left, int right){
+        while(left<right){
+            if(s.charAt(left)!=s.charAt(right))
+                return false; 
+            
+            left++; 
+            right--; 
+        }
+
+        return true; 
+    }
+}
+
+//Time complexity = O(n)
+//Space complexity = O(1)
