a14d19387c46689d62c198dd3786905e944be08d
Daksh Dharmani <daksh.v.dharmani@gmail.com>
2026-08-14 09:45:02 +0530

    Sliding window with frequency array

---
+++ TwoPtr/VariableSlidingWindow/Leetcode3090/OptimalSol.java
@@ -0,0 +1,27 @@
+//Optimal Solution 
+
+class Solution {
+public:
+    int maximumLengthSubstring(string s) {
+        vector<int> freq(26, 0);   
+        int left = 0;              
+        int maxLen = 0;        
+
+        for (int right = 0; right < s.size(); right++) {
+        
+            freq[s[right] - 'a']++;
+
+            while (freq[s[right] - 'a'] > 2) {
+                freq[s[left] - 'a']--;
+                left++;
+            }
+
+            maxLen = max(maxLen, right - left + 1);
+        }
+
+        return maxLen;
+    }
+};
+
+//Time complexity = O(n) <-iterates entire array 
+//Space complexity = O(1) <-the freq array will always be of size 26 
