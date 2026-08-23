f8fc3848de99a93a7b306e59b4cb1f651d018b27
Daksh Dharmani <daksh.v.dharmani@gmail.com>
2026-08-23 20:43:31 +0530

    Sqrt(x) in java with binary search

---
+++ Arrays/BinarySearch/Leetcode69/OptimalSol.java
@@ -0,0 +1,15 @@
+//Optimal solution 
+
+class Solution {
+    public int mySqrt(int x) {
+        long num = x; 
+
+        while(num*num>x)
+            num = (num + x/num)/2; 
+        
+        return (int)num; 
+    }
+}
+
+//Time complexity = O(nlogn)
+//Space complexity = O(1)
