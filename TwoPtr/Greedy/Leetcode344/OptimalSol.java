e579d01775ede2c3cd896782c7efa59ec33ed6f7
Daksh Dharmani <daksh.v.dharmani@gmail.com>
2026-09-23 23:56:06 +0530

    Fix formatting issue in reverseString method

--- TwoPtr/Greedy/Leetcode344/OptimalSol.java
+++ TwoPtr/Greedy/Leetcode344/OptimalSol.java
@@ -4,7 +4,7 @@ class Solution {
     public void reverseString(char[] s) {
         int left = 0, right = s.length-1; 
 
-        while(left<=right){
+        while(left<=right){ 
             char temp = s[left]; 
             s[left] = s[right]; 
             s[right] = temp;  
